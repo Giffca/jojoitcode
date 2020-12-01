f2e944f4b0a648b20fb99bfd3424c3fa767a1b22
Giffca <Tiimmy@hotmail.se>
2020-12-01 22:24:09 +0100

    images

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,8 +6,8 @@ export default function Navbar(){
         <div className="nav-bar font-normal">
             <div className="titel"><Link to ={`/`}><span><p>JoJo IT Handelsbolag</p></span></Link></div>
         <div className="nav-bar-buttons">
-            <div className="box-effect"><Link to ={`/previousworks`}><span><p>Våra verk</p></span></Link></div>
             <div className="box-effect"><Link to ={`/about`}><span><p>Om oss</p></span></Link></div>
+            <div className="box-effect"><Link to ={`/previousworks`}><span><p>Våra verk</p></span></Link></div>
             <div className="box-effect"><Link to ={`/contact`}><span><p>Kontakt</p></span></Link></div>
         </div>
             
