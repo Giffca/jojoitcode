91a7851601162f512fa5af0fde49fc2fbcdbadd6
Giffca <Tiimmy@hotmail.se>
2020-12-14 04:35:47 +0100

    Isometriska bilder

---
+++ src/components/isometricimage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import '../styles/isometricimage.css'
+
+export default function IsometricImage({children}){
+    return (
+        <div class="image">{children}</div>
+
+    )
+}
--- src/pages/previousworks.js
+++ src/pages/previousworks.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
+import IsometricImage from "../components/isometricimage"
 import JoJoIT from "../images/jojoit.png"
 import MalinsGarderob from "../images/malinsgarderob.png"
 import NaturalHealing from "../images/naturalhealing.png"
@@ -11,17 +12,37 @@ export default function Contact() {
   <div>
     <Layout>
     <div className="scrollable">
-      <p className="left"><h2>Våra Verk</h2>2020</p>
+    <h2 className="preamble">Våra Verk</h2>
+      <p className="left">2020</p>
     <ul className="center">
-      <li><h2 className="font-normal">JoJo IT</h2> <a className="v2" href="https://github.com/Giffca/jojoitcode" target="_blank" rel="noreferrer">https://github.com/Giffca/jojoitcode</a> <br></br><div className="flex"><a className="tooltip" target="_blank" rel="noreferrer" href={JoJoIT}><img alt="jojoit" src={JoJoIT} className="tooltipimage"></img> Bild</a> Gjord med hjälp av Gatsby </div></li>
-      <li><h2 className="font-normal">Malins Garderob Huskvarna</h2> <a className="v2" href="https://www.malinsgarderob.com/" target="_blank" rel="noreferrer">https://www.malinsgarderob.com/</a> <br></br><div className="flex"><a className="tooltip" target="_blank" rel="noreferrer" href={MalinsGarderob}><img alt="malinsgarderob" src={MalinsGarderob} className="tooltipimage"></img> Bild</a> Gjord med hjälp av WordPress.</div></li>
-      </ul>
-      <p className="left"> 2019 </p>
-      <ul className="center bottomfill">
-      <li><h2 className="font-normal">ProteinBro</h2> <a className="v2" href="https://webbkurs.ei.hv.se/~tijo0010/wpTimmyCMS/" target="_blank" rel="noreferrer">https://webbkurs.ei.hv.se/~tijo0010/wpTimmyCMS/</a> <br></br><div className="flex"><a className="tooltip" target="_blank" rel="noreferrer" href={ProteinBro}><img alt="proteinbro" src={ProteinBro} className="tooltipimage"></img> Bild</a> Skoluppgift gjord med hjälp av Elementor/WordPress </div></li>
-      <li><h2 className="font-normal">Natural Healing</h2> <a className="v2" href="https://webbkurs.ei.hv.se/~anjo0199/CMS200/" target="_blank" rel="noreferrer">https://webbkurs.ei.hv.se/~anjo0199/CMS200/</a> <br></br><div className="flex"><a className="tooltip" target="_blank" rel="noreferrer" href={NaturalHealing}><img alt="naturalhealing" src={NaturalHealing} className="tooltipimage"></img> Bild</a> Skoluppgift gjord med hjälp av Elementor/WordPress </div></li>
-      </ul>
-      </div>
+
+    <li>
+    <IsometricImage><a href="https://github.com/Giffca/jojoitcode" target="_blank" rel="noreferrer"><img alt="jojoit" src={JoJoIT}></img></a></IsometricImage>
+    <div className="pt"><h2 className="font-normal">JoJo IT</h2> Gjord med Gatsby</div>
+    </li>
+
+    <li>
+      <IsometricImage><a href="https://www.malinsgarderob.com/" target="_blank" rel="noreferrer"><img alt="malinsgarderob" src={MalinsGarderob}></img></a></IsometricImage>
+      <div className="pt"><h2 className="font-normal">Malins Garderob</h2> Gjord med Elementor/WordPress.</div>
+    </li>
+
+    <li>
+    </li>
+    </ul>
+    <p className="left"> 2019 </p>
+    <ul className="center">
+    <li>
+      <IsometricImage><a href="https://webbkurs.ei.hv.se/~tijo0010/wpTimmyCMS/" target="_blank" rel="noreferrer"><img alt="malinsgarderob" src={ProteinBro}></img></a></IsometricImage>
+      <div className="pt"><h2 className="font-normal">ProteinBro</h2> Skoluppgift gjord med <br></br>Elementor/WordPress</div>
+    </li>
+
+    <li>
+      <IsometricImage><a href="https://webbkurs.ei.hv.se/~anjo0199/CMS200/" target="_blank" rel="noreferrer"><img alt="malinsgarderob" src={NaturalHealing}></img></a></IsometricImage>
+      <div className="pt"><h2 className="font-normal">Natural Healing</h2> Skoluppgift gjord med <br></br>Elementor/WordPress</div>
+    </li>
+
+    </ul>
+    </div>
     </Layout>
   </div>
   ) 
