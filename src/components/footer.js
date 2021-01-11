b74d22d32c2522e49994c6923103b564b3863af6
Giffca <Tiimmy@hotmail.se>
2021-01-11 01:35:22 +0100

    footer+

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,9 @@ import React from 'react'
 
 export default function Footer(){
     return <footer className="footer font-normal">
+        <div className="footerwrapper">
         <div className="footertext">jojoit.contact@gmail.com</div> 
-         <div className="footertext">JoJo IT Handelsbolag © 2020</div> 
+        <div className="footertext">JoJo IT Handelsbolag © 2021</div>
+        </div>
          </footer>
 }
