a68466f11eb092a198cec947a0e943246c802029
Anton Josefsson <anton.josefsson.94@hotmail.com>
2021-01-12 21:34:31 +0100

    AwesomeFont

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Contact() {
   return (
-    
+     
   <div>
     <Layout>
     <div className="contactBox">
       <h2 className="center">Kontaktuppgifter</h2>
       <div className="contactText">jojoit.contact@gmail.com  </div>
       <div className="contactText">076-123 45 67</div>
+      <FontAwesomeIcon icon={['fab', 'linkedin']} size="3x"/>
     </div>
     </Layout>
   </div>
