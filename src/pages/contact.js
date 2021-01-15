0ef711897c13c98ccecdb24396437372c0f3befb
Anton Josefsson <anton.josefsson.94@hotmail.com>
2021-01-15 15:35:29 +0100

    Kontakt Update

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,15 +8,21 @@ export default function Contact() {
      
   <div>
     <Layout>
-    <div className="contactBox">
-      <h2 className="center">Kontaktuppgifter</h2>
-      <div className="contactText">jojoit.contact@gmail.com  </div>
-      <div className="contactText">076-123 45 67</div>
-      <FontAwesomeIcon icon={['fab', 'linkedin']} size="6x"/>
-      <FontAwesomeIcon icon={['fab', 'apple']} size="3x"/>
+    <div className="contactWrapper">
+      <div className="contactLeft">
+        <h2 className="contactLeftHeader">Kontakta oss!</h2>
+        <div className="contactLeftText">Vid funderingar eller för kostnadsförslag tveka inte på att höra av er.</div>
+        <div className="contactLeftLinkedText">Vi finns även på LinkedIn!</div>
+        <a className="contactLink" href="https://se.linkedin.com/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"/></a>
+      </div>
+      <div className="contactRight">
+        <div className="contactBox">
+          <h2 className="contactRightHeader">Kontaktuppgifter</h2>
+          <FontAwesomeIcon icon={['fas', 'envelope']} size="2x"/><div className="contactRightText"><a href="mailto:jojoit.contact@gmail.com">jojoit.contact@gmail.com</a></div>
+          <FontAwesomeIcon icon={['fas', 'phone']} size="2x"/><div className="contactRightText"><a href="tel:076-123457"> 076-123 45 67</a></div>
+        </div>
+      </div>
     </div>
-    
-    
     </Layout>
   </div>
   ) 
