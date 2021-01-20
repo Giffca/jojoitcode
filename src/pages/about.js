59a6d232fd12c06e044b285e99e945e75777d317
Giffca <Tiimmy@hotmail.se>
2021-01-20 22:09:46 +0100

    slideshow improvements + package upgrades

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,9 @@ import React from "react"
 import Layout from "../components/layout"
 import img1 from "../images/about1.jpg"
 import img2 from "../images/about2.png"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function About() {
   return (
-    
   <div>
     <Layout>
     <div>
@@ -16,7 +14,7 @@ export default function About() {
     <article className="breadText"><span className="bigger names">Timmy Johansson&nbsp;&nbsp;&nbsp;&nbsp;&&nbsp;&nbsp;&nbsp;&nbsp;Anton Josefsson </span><br></br><br></br>Vi är ett företag baserat i Jönköping som är grundat av två stycken nyutbildade webmasters. Vi lärde känna varandra i början av våra studier, där vårat gemensamma intresse för webbutveckling förde oss samman.<br></br><br></br> Tillsammans har vi sedan dess studerat igenom programmen:<br></br><br></br><span className="bigger">"Mjukvaruutveckling och mobila plattformar"</span><br></br><span className="smaller"><i>Jönköping University</i><br></br>2014-2016</span><br></br><br></br><span className="bigger">"Webbutvecklare" & "Webmaster"</span><br></br><span className="smaller"><i>Högskolan Väst</i><br></br>2017-2020</span><br></br><br></br>
     </article></div>
     <div className="flex bottomfill">
-    <article className="breadText"><h2>Våra Kunskaper</h2><br></br>Genom våra utbildningar så har vi fått en bred kunskap inom programmering, webbutveckling samt webbdesign. <br></br><br></br> <div className="flex-evenly"><p><span className="bigger kunskap"> Språk </span><br></br><span className="smaller"><FontAwesomeIcon icon={['fab', 'js']}/> JavaScript<br></br><FontAwesomeIcon icon={['fab', 'java']}/> Java<br></br><FontAwesomeIcon icon={['fab', 'python']}/> Python<br></br><FontAwesomeIcon icon={['fab', 'php']}/> PHP<br></br>C#<br></br>C++<br></br>SQL</span></p><p><span className="bigger kunskap"> Miljöer </span><br></br><span className="smaller"><FontAwesomeIcon icon={['fab', 'react']}/> React<br></br><FontAwesomeIcon icon={['fab', 'wordpress-simple']}/> WordPress<br></br><FontAwesomeIcon icon={['fab', 'node-js']}/> Node.js<br></br><FontAwesomeIcon icon={['fab', 'angular']}/> Angular<br></br>.NET<br></br>MySQL<br></br>Photoshop</span></p></div> <br></br>Vi strävar efter att konstant hålla oss uppdaterade och införskaffar därför hela tiden ny kunskap om relevanta miljöer och språk.</article><img src={img2} alt="kod"></img> </div> 
+    <article className="breadText"><h2>Våra Kunskaper</h2><br></br>Genom våra utbildningar så har vi fått en bred kunskap inom programmering, webbutveckling samt webbdesign. <br></br><br></br> <div className="flex-evenly"><p><span className="bigger kunskap"> Språk </span><br></br><span className="smaller">JavaScript<br></br>Java<br></br>Python<br></br>PHP<br></br>C#<br></br>C++<br></br>SQL</span></p><p><span className="bigger kunskap"> Miljöer </span><br></br><span className="smaller">React<br></br>WordPress<br></br>Node.js<br></br>Angular<br></br>.NET<br></br>MySQL<br></br>Photoshop</span></p></div> <br></br>Vi strävar efter att konstant hålla oss uppdaterade och införskaffar därför hela tiden ny kunskap om relevanta miljöer och språk.</article><img src={img2} alt="kod"></img> </div> 
     <div className="bottomfill"></div>
     </div>
     </Layout>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,43 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import 'react-slideshow-image/dist/styles.css'
-import { Slide } from 'react-slideshow-image';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { Slide } from "react-slideshow-image";
+import "react-slideshow-image/dist/styles.css"
 
 export default function Home() {
   return (
   
   <div>
     <Layout>
-
     <div className="startpagepicture">
-
-    <h2 className="center">Vi kan</h2>
-    <div>
-        <Slide easing="ease" autoplay="true">
-          <div className="each-slide">
-            <div>
-              <span>Slide 1</span><br></br>
-              <FontAwesomeIcon icon={['fab', 'react']} size="9x"/>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div>
-              <span>Slide 2</span><br></br>
-              <FontAwesomeIcon icon={['fab', 'angular']} size="9x"/>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div>
-              <span>Slide 3</span><br></br>
-              <FontAwesomeIcon icon={['fab', 'node-js']} size="9x"/>
-            </div>
-          </div>
-        </Slide>
-      </div>
-
-
-
       <div className="Iam">
         <p>Vi kan</p>
         <div className="scrolling">
@@ -48,16 +20,35 @@ export default function Home() {
             Databaser<br />
             JavaScript Ramverk <br/>
             WordPress
-
             </div>
         </div>
       </div>
     </div>
 
+      
+      
+      
+      
+
     <div className="startpage"><br></br><br></br><br></br><h2 className="center">Vi är tillgängliga för arbete som webmasters<br></br> samt letar efter spännande projekt att jobba inom.<br></br>Helst på distans.</h2></div>
     
 
 
+
+    <div className="startpage">
+        <Slide easing="ease" arrows={false} duration={4000} transitionDuration={1800}>
+          <div className="each-slide">
+            <FontAwesomeIcon icon={['fab', 'angular']} size="8x"/>
+              <FontAwesomeIcon icon={['fab', 'react']} size="8x"/>
+              <FontAwesomeIcon icon={['fab', 'node-js']} size="8x"/>
+          </div>
+          <div className="each-slide">
+              <FontAwesomeIcon icon={['fab', 'php']} size="8x"/>
+              <FontAwesomeIcon icon={['fab', 'js']} size="8x"/>
+              <FontAwesomeIcon icon={['fab', 'wordpress-simple']} size="8x"/>
+          </div>
+        </Slide>
+      </div>
 
     
 
