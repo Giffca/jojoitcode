684a1cbdcd56900f8fe0bb592f822b26af6efc72
Anton Josefsson <anton.josefsson.94@hotmail.com>
2021-01-24 15:31:57 +0100

    bl.a. 1080 style fixes

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,7 @@ export default function Contact() {
      
   <div>
     <Layout>
-    <div className="contactWrapper">
+    <div className="contactWrapper contactPadding">
       <div className="contactLeft">
         <h2 className="contactLeftHeader">Kontakta oss!</h2>
         <div className="contactLeftText">Vid funderingar eller för kostnadsförslag tveka inte på att höra av er.</div>
@@ -18,8 +18,8 @@ export default function Contact() {
       <div className="contactRight">
         <div className="contactBox">
           <h2 className="contactRightHeader">Kontaktuppgifter</h2>
-          <FontAwesomeIcon icon={['fas', 'envelope']} size="2x"/><div className="contactRightText"><a href="mailto:jojoit.contact@gmail.com">jojoit.contact@gmail.com</a></div>
-          <FontAwesomeIcon icon={['fas', 'phone']} size="2x"/><div className="contactRightText"><a href="tel:076-123457"> 076-123 45 67</a></div>
+          <a href="mailto:jojoit.contact@gmail.com"><FontAwesomeIcon icon={['fas', 'envelope']} size="2x"/><div className="contactRightText">jojoit.contact@gmail.com</div></a>
+          <a href="tel:076-123457"><FontAwesomeIcon icon={['fas', 'phone']} size="2x"/><div className="contactRightText"> 076-123 45 67</div></a>
         </div>
       </div>
     </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -9,30 +9,31 @@ export default function Home() {
   
   <div className="startpage">
     <Layout>
-      <div className="startpagepicture">
-        <h2 className="center slideshowHeader">Kunskaper inom</h2>
-        <Slide easing="ease" arrows={false} pauseOnHover={false} canSwipe={false} duration={3000} transitionDuration={1000}>
-              <div className="each-slide">
-                <div className="slideshowText">Webbutveckling</div>
-              </div>
-              <div className="each-slide">
-                <div className="slideshowText">Webbdesign </div>
-              </div>
-              <div className="each-slide">
-                <div className="slideshowText">JavaScript Ramverk </div>
-              </div>
-              <div className="each-slide">
-                <div className="slideshowText">Content Management Systems</div>
-              </div>
-              <div className="each-slide">
-                <div className="slideshowText">Databashantering </div>
-              </div>
-              <div className="each-slide">
-                <div className="slideshowText">Mjukvaruutveckling </div>
-              </div>
-        </Slide>
+      <div className="topSlideWrapper">
+        <div className="startpagepicture">
+          <h2 className="center slideshowHeader">Kunskaper inom</h2>
+          <Slide easing="ease" arrows={false} pauseOnHover={false} canSwipe={false} duration={3000} transitionDuration={1000}>
+                <div className="each-slide">
+                  <div className="slideshowText">Webbutveckling</div>
+                </div>
+                <div className="each-slide">
+                  <div className="slideshowText">Webbdesign </div>
+                </div>
+                <div className="each-slide">
+                  <div className="slideshowText">JavaScript Ramverk </div>
+                </div>
+                <div className="each-slide">
+                  <div className="slideshowText">Content Management Systems</div>
+                </div>
+                <div className="each-slide">
+                  <div className="slideshowText">Databashantering </div>
+                </div>
+                <div className="each-slide">
+                  <div className="slideshowText">Mjukvaruutveckling </div>
+                </div>
+          </Slide>
+        </div>
       </div>
-
       <div className="contactWrapper">
         <div className="contactLeft">
         <h2 className="overflow">IT- och konsult-tjänster med fokus på webbteknik</h2>
@@ -41,11 +42,10 @@ export default function Home() {
         <div className="contactLeft tiltright">
             <a href="mailto:jojoit.contact@gmail.com"> <FontAwesomeIcon icon={['fas', 'envelope']} size="2x"/><div className="contactRightText">jojoit.contact@gmail.com</div></a>
             <a href="https://se.linkedin.com/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn"><FontAwesomeIcon icon={['fab', 'linkedin']} size="2x"/><div className="contactRightText">JoJo IT</div></a>
-            
         </div>
       </div>
 
-      <div className="bottomfill">
+      <div className="bottomSlideWrapper">
           <Slide easing="ease" arrows={false} pauseOnHover={false} canSwipe={false} duration={4000} transitionDuration={1800}>
             <div className="each-slide">
               <FontAwesomeIcon icon={['fab', 'angular']} size="8x"/>
