25137f36271ce15b0976c49bc944f7513680bd17
Giffca <Tiimmy@hotmail.se>
2021-02-15 19:05:11 +0100

    mobile about

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(){
         <nav className="nav-bar font-normal">
         <div className="navwrapper">
             <div className="titel"><Link to ={`/`}><span><h1 className="titel">JoJo IT Handelsbolag</h1></span></Link></div>
-        <div className="flex-evenly">
+        <div className="nav flex-evenly">
             <div className="box-effect"><Link to ={`/about`}><span><p>Om oss</p></span></Link></div>
             <div className="box-effect"><Link to ={`/previousworks`}><span><p>Våra verk</p></span></Link></div>
             <div className="box-effect"><Link to ={`/contact`}><span><p>Kontakt</p></span></Link></div>
