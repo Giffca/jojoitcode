227c850213bfc0e4b05533e070d2e209e86918b4
Giffca <Tiimmy@hotmail.se>
2022-05-04 22:45:17 +0200

    reacthelmet

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Helmet } from "react-helmet"
 import Navbar from "./navbar"
 import Footer from "./footer"
 import "./fontawesome"
@@ -7,6 +7,10 @@ import '../styles/index.css'
 
 export default function Layout({children}){
     return <div className="pageContainer" id="outer-container">
+        <Helmet htmlAttributes={{ lang: 'sv-SE'}}>
+          <title>JoJo IT</title>
+          <link rel="canonical" href="https://jojoit.tk" />
+        </Helmet>
     <Navbar/>
         <div className="wrapper" id="page-wrap">
             {children}
