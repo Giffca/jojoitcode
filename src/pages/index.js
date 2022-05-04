ff9cacb80c30ffa8c2b58901b489c156c6691b67
Giffca <Tiimmy@hotmail.se>
2022-05-04 23:16:36 +0200

    pageloadlayoutshift

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react"
+
+// The following import prevents a Font Awesome icon server-side rendering bug,
+// where the icons flash from a very large icon down to a properly sized one:
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import "react-slideshow-image/dist/styles.css"
+// Prevent fontawesome from adding its CSS since we did it manually above:
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; /* eslint-disable import/first */
+
 import Layout from "../components/layout"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { Slide } from "react-slideshow-image";
-import "react-slideshow-image/dist/styles.css"
+
+
 
 export default function Home() {
   return (
